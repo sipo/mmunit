edb0861a96f26b0d9fc94172951b324bc735f20f
sipo <github@sipo.jp>
2014-02-09 00:08:33 +0900

    コメント等整理

--- src/jp/sipo/mmunit/MAssert.hx
+++ src/jp/sipo/mmunit/MAssert.hx
@@ -2,7 +2,6 @@ package jp.sipo.mmunit;
 /**
  * 今のところマクロ専用。
  * エラーをキャッチし、呼び出し元関数の位置としてコンソール警告を出す
- * posInfosToPositionはnanoTestを参考にした
  * 
  * @auther sipo
  */
@@ -13,22 +12,31 @@ import haxe.io.Bytes;
 import massive.munit.Assert;
 class MAssert
 {
+	/**
+	 * Assert.areEqualを実行
+	 */
 	public static function areEqual(expected:Dynamic, actual:Dynamic, ?pos:PosInfos)
 	{
 		try{
-			Assert.areEqual($v{expected}, $v{actual});
+			Assert.areEqual(expected, actual);
 		}catch (error:Dynamic){
 			Context.warning(Std.string(error), posInfosToPosition(pos) );
 		}
 	}
 	
+	/**
+	 * PosInforをマクロ用に変換
+	 * nanoTestを参考に、ほぼ真似
+	 * @param	pos
+	 */
 	static function posInfosToPosition(pos:PosInfos) 
 	{
-		var file = File.read( pos.fileName ).readAll().toString();
+		var file = File.read(pos.fileName).readAll().toString();
 		var ereg = ~/(\r\n|\r|\n)/;
 		
 		var min = 0;
-		for ( i in 0...pos.lineNumber - 1 ) {
+		for (i in 0...pos.lineNumber - 1) 
+		{
 			ereg.match( file );
 			var pos = ereg.matchedPos();
 			min += pos.pos + pos.len;
@@ -36,12 +44,13 @@ class MAssert
 		}
 		
 		var max = min + 
-			if( ereg.match( file ) )
+			if(ereg.match( file ))
 				ereg.matchedPos().pos;
 			else 
 				file.length;
 				
-		return Context.makePosition( {
+		return Context.makePosition
+		({
 			file : pos.fileName, min : min, max : max,
 		});
 	}
